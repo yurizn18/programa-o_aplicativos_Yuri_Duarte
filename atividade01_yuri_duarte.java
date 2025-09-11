03e9f10008b070aa093dbfee55aac529ac420dac
yurizn18 <duarteyurioliveira@gmail.com>
2025-09-11 20:27:09 -0300

    Add files via upload

---
+++ atividade01_yuri_duarte.java
@@ -0,0 +1,35 @@
+public class Main {
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+  //       String nome;
+		// int idade;
+		// double altura;
+		// boolean estudante;
+		// nome="yuri";
+		// idade= 18;
+		// altura=1.67;
+		// estudante= true;
+  //       System.out.println(nome);
+  //       System.out.println(idade);
+  //       System.out.println(altura);
+  //       System.out.println(estudante);
+
+        int a;
+        int b;
+        int c;
+        int d;
+        a=25;
+        b=(a*3);
+        c=10;
+        d= a/c;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+}
