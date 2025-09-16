8245473f3c255b98dee97206cf2149a0b2329772
yurizn18 <duarteyurioliveira@gmail.com>
2025-09-15 22:32:56 -0300

    Add files via upload

---
+++ Atividade02_yuri_and_gabriel (1).java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade02_yuri_and_gabriel;
+import java.util.Scanner;
+import java.lang.Math;
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade02_yuri_and_gabriel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //System.out.print("Hello World!");
+        //System.out.println("Hello World!");
+        //System.out.println("Hello\n World!");
+        //System.out.println("Hello\t World!");
+        
+   
+      
+       //double raio,area,circuferencia;
+       
+       //System.out.println("digite o valor do raio da circuferencia");
+       //raio= input.nextDouble();
+       
+       //area= Math.PI*(Math.pow(raio,2));
+       //circuferencia=2*Math.PI*raio;
+       
+       //System.out.println("o resultado da area é:" +area);
+            //System.out.println("o resultado da circuferencia é:" +circuferencia);
+       
+       
+       
+       
+        
+        // TODO code application logic here
+        //Scanner input=new Scanner(System.in);
+       
+       //double peso, altura,imc;
+       //System.out.println("digite o valor do peso");
+       //peso= input.nextDouble();
+           //System.out.println("digite o valor da altura");
+          //altura= input.nextDouble();
+          //imc= peso/(altura*altura);
+          //System.out.println("o resultado do imc é:" +imc);
+          //Scanner input=new Scanner(System.in);
+          //double a, b,c;
+          //System.out.println("digite o valor de b");
+          //b= input.nextDouble();
+          //System.out.println("digite o valor de c");
+          //c= input.nextDouble();
+          //a= Math.sqrt(Math.pow(b,2)+ Math.pow(c,2));
+          //System.out.printf(" o resultado de a é: %.3f",a);
+          
+          //Scanner input=new Scanner(System.in);
+          //double real, dolar,resultado, troco;
+           //dolar=5.32;
+          //System.out.println("digite o valor do real ");
+          //real= input.nextDouble();
+          //resultado=Math.floor (real/dolar);
+          //System.out.println("o resultado e :"+ resultado);
+        //troco = real - (resultado * dolar);
+        //System.out.println("O valor em dólares e: " + resultado);
+        //System.out.println("O troco em reais e: " + troco);
+
+      
+            
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+                  
+          
+          
+          
+          
+          
+         
+          
+         
+                 
+       
+       
+    }
+          
+    
+}
