622dd43765156461fcb90d09107a907908a4f000
yurizn18 <duarteyurioliveira@gmail.com>
2025-09-16 21:08:50 -0300

    Add files via upload

---
+++ Atividade02_yuri_&_gabriel.java
@@ -0,0 +1,144 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade02_yuri_and_gabriel;
+import java.util.Scanner;
+import java.lang.Math;
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade02_yuri_and_gabriel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //System.out.print("Hello World!");
+        //System.out.println("Hello World!");
+        //System.out.println("Hello\n World!");
+        //System.out.println("Hello\t World!");
+        
+   
+      
+       //double raio,area,circuferencia;
+       
+       //System.out.println("digite o valor do raio da circuferencia");
+       //raio= input.nextDouble();
+       
+       //area= Math.PI*(Math.pow(raio,2));
+       //circuferencia=2*Math.PI*raio;
+       
+       //System.out.println("o resultado da area é:" +area);
+            //System.out.println("o resultado da circuferencia é:" +circuferencia);
+       
+       
+       
+       
+        
+        // TODO code application logic here
+        //Scanner input=new Scanner(System.in);
+       
+       //double peso, altura,imc;
+       //System.out.println("digite o valor do peso");
+       //peso= input.nextDouble();
+           //System.out.println("digite o valor da altura");
+          //altura= input.nextDouble();
+          //imc= peso/(altura*altura);
+          //System.out.println("o resultado do imc é:" +imc);
+          
+          
+          //Scanner input=new Scanner(System.in);
+          
+          //double a, b,c;
+          //System.out.println("digite o valor de b");
+          //b= input.nextDouble();
+          //System.out.println("digite o valor de c");
+          //c= input.nextDouble();
+          //a= Math.sqrt(Math.pow(b,2)+ Math.pow(c,2));
+          //System.out.printf(" o resultado de a é: %.3f",a);
+          
+          //Scanner input=new Scanner(System.in);
+          //double real, dolar,resultado, troco;
+           //dolar=5.32;
+          //System.out.println("digite o valor do real ");
+          //real= input.nextDouble();
+          //resultado=Math.floor (real/dolar);
+          //System.out.println("o resultado e :"+ resultado);
+        //troco = real - (resultado * dolar);
+        //System.out.println("O valor em dólares e: " + resultado);
+        //System.out.println("O troco em reais e: " + troco);
+
+        
+        //5 
+        //Scanner input=new Scanner(System.in);
+          //int n,j1,j2,s;
+          //n=input.nextInt();
+          //j1=input.nextInt();
+          //j2=input.nextInt();
+          //s=(j1+j2)/n;
+          
+          //System.out.print("a média é"+s);
+          //System.out.print(Math.max(j1,j2));
+          //System.out.println(Math.min(j1,j2));
+           
+          //6
+          //Scanner input=new Scanner(System.in);
+         // double graus, tan,sen,cos, rady;
+         // graus=input.nextDouble();
+         // rady=Math.toRadians(graus);
+         // sen=Math.sin(rady);
+         // cos=Math.cos(rady);
+         // tan=Math.tan(rady);
+         // System.out.println("tang"+tan+ "cos"+cos+ "seno"+sen);
+         //7
+         //Scanner input=new Scanner(System.in);
+        // double x1,x2,y1,y2,calc;
+         //x1=input.nextDouble();
+          // x2=input.nextDouble();
+         //  y1=input.nextDouble();
+         //y2=input.nextDouble();
+        // calc=Math.sqrt(Math.pow((x2-x1),2))+ (Math.pow((y2-y1),2));
+        //8
+        
+        
+        Scanner input=new Scanner(System.in);
+        // double n1,n2,n3,m,s,d;
+        // System.out.println("digite o valor de n1");
+         //n1=input.nextDouble();
+        // System.out.println("digite o valor de n2");
+        // n2=input.nextDouble();
+        // System.out.println("digite o valor de n3");
+        // n3=input.nextDouble();
+         //m=(n1+n2+n3)/3;
+        // s= Math.abs(n1)+ Math.abs(n2)+ Math.abs(n3);
+        // d= Math.abs(n1-n3);
+        //System.out.println("a diferença é"+d);
+ 
+
+       
+         //9
+         
+        // int a,b,c;
+        // double x1,x2, delta;
+         //a=input.nextInt();
+        // b=input.nextInt();
+        // c=input.nextInt();
+       
+        //delta=Math.sqrt((Math.pow(b,2))-(4*a*c));
+       // x1=(0-b)+(delta)/(2*a);
+        // x2=(0-b)+(delta)/(2*a);
+        
+        //10
+        double far,cel;
+        System.out.print("digite o valor de far:");
+        far=input.nextDouble();
+        cel=(far-32)/1.0;
+   System.out.println("O valor de " + far + "°F em Celsius é: " + cel + "°C");
+          
+       
+    }
+          
+    
+}
