7d8023767d6d8ffd7afaf9ccd630a47d23a5bea8
yurizn18 <duarteyurioliveira@gmail.com>
2025-09-19 21:25:08 -0300

    Add files via upload

---
+++ Atividade04.java
@@ -0,0 +1,362 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade03;
+
+/**
+ *
+ * @author Yuri
+ */
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Atividade03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       Scanner entrada=new Scanner(System.in);
+      //  int acucar,p,t;
+      //  acucar=200/8;
+       // System.out.println("digite a quantidade de porções");
+      //  p=entrada.nextInt();
+        
+       // t=(p*acucar);
+       // System.out.print("serão necessários"+t+"gr de acucar");
+        
+        
+        //2
+        //int n,paginas,minutos,total;
+        //System.out.print("digite a quantidade de impresoras que deseja usar");
+        //n= entrada.nextInt();
+       // paginas=7;
+       // minutos=(n*paginas);
+        //total=180/(minutos);
+       // System.out.println("total:" +total+"minutos"+minutos);
+        
+        //3 
+      //  int d;
+      //  double km_litro,total_litro;
+      //  km_litro=13;
+      //  System.out.println("digite a distancia(km):");
+      //  d=entrada.nextInt();
+      //  total_litro= (d/km_litro);
+        
+       // System.out.print("o total  em litros é"+total_litro);
+        
+      //  int n;
+     //  System.out.println("digite o numero: ");
+     // n=entrada.nextInt();
+        // if (n < 0) {
+        //   System.out.println("Esse número é negativo");
+       //  }else {
+      //     System.out.println("Esse número é positivo");
+     //  }
+      
+      ///2
+      
+      //     int n;
+     //  System.out.println("digite o numero: ");
+    //  n=entrada.nextInt();
+      //   if (n < 0) {
+      //     System.out.println("Esse número é negativo");
+      //     } else if (n == 0) {
+      //     System.out.println("Esse número é zero");
+    //    }
+     //     else {
+     //      System.out.println("Esse número é positivo");
+     //  }
+      
+      
+       
+       //3
+                
+      // int n1,n2,n3;
+       //double  media;
+     //  System.out.println("digite a nota1: ");
+     //  n1=entrada.nextInt();
+      //   System.out.println("digite a nota2: ");
+      // n2=entrada.nextInt();
+      //  System.out.println("digite a nota3: ");
+     //  n3=entrada.nextInt();
+       
+     //  media= (n1+n2+n3)/3;
+       // System.out.println("a media e: "+media);
+      //  
+     //   if(media>=7){
+       //     System.out.print("aptovado");
+     //   } else {
+          //  System.out.print("reprovado");
+     //   }
+                
+        
+        //4
+        //   int n1,n2,n3;
+      // double  media;
+     // System.out.println("digite a nota1: ");
+     // n1=entrada.nextInt();
+     //    System.out.println("digite a nota2: ");
+     //  n2=entrada.nextInt();
+     //   System.out.println("digite a nota3: ");
+     // n3=entrada.nextInt();
+       
+     //  media= (n1+n2+n3)/3;
+     //   System.out.println("a media e: "+media);
+        
+     //   if(media>=7){
+        //    System.out.print("aprovado");
+     //   } else if(media>=5&& media<7){
+     //   System.out.print("alauno em recuperação");
+     //   } else {
+      //      System.out.print("reprovado");
+     //   }
+    
+     //5
+    
+   //  int n;
+    // System.out.print("digite um numero");
+          //   n=entrada.nextInt();
+          //   if(n%2==0){
+           //      System.out.print("o numero é par");
+          //   } else{
+          //       System.out.print("esse numero é impar");
+             //}
+             
+             //6
+             
+        //     int n1,n2,n3;
+        //     System.out.println("digite a nota1: ");
+    // n1=entrada.nextInt();
+      //   System.out.println("digite a nota2: ");
+      // n2=entrada.nextInt();
+     //  System.out.println("digite a nota3: ");
+      //n3=entrada.nextInt();
+       
+     
+          
+      
+      //  if (n1 >= n2 && n1 >= n3) {
+        //    System.out.println("O maior número é: " + n1);
+      //  } else if (n2 >= n1 && n2 >= n3) {
+        //    System.out.println("O maior número é: " + n2);
+     //   } else {
+      //      System.out.println("O maior número é: " + n3);
+        //}
+
+             //7
+             
+       //      int idade;
+      //         System.out.println("digite a sua idade: ");
+    // idade=entrada.nextInt();
+  //  if(idade>=18){
+   //     System.out.print("Adulto");
+   // }else if (idade>=14){
+   //     System.out.print("juvenil");
+        
+ //   }else if (idade>=9){
+     //   System.out.print("infantil");
+ //   } else{
+  //      System.out.print("mirim");
+    //}
+    
+             //8
+             
+           //  int n1,n2, soma, calc;
+           //  System.out.print("digite o numero 1: ");
+          //   n1=entrada.nextInt();
+           //   System.out.print("digite o numero 2: ");
+           //  n2=entrada.nextInt();
+             
+           //  soma=(n1+n2);
+          //   System.out.println("a soma e : "+ soma);
+             
+          //   if(soma>20){
+         //  calc= soma+8;
+                 
+         //    }else{
+          //       calc= soma-5;
+           //  }
+         //    System.out.println("O resultado final é: " + calc);
+
+             //9
+                  //  double v_p,calc;
+                //    System.out.print("digite o valor do produto: ");
+           //  v_p=entrada.nextDouble();
+           //  if(v_p<20){
+             //   calc= v_p+(v_p*0.45);
+                
+            // }else{
+                 
+             //   calc=v_p+(v_p*0.30);
+            //  }
+            ///  System.out.println("O resultado final é: " + calc);
+            ///
+              //10
+          //   int quantidade_macas;
+          //   double calc;
+             
+          //   System.out.print("digite a quantidade de macas: ");
+          //   quantidade_macas =entrada.nextInt();
+             
+         //    if (quantidade_macas<12){
+          //       calc= quantidade_macas*0.30;
+                 
+        //     } else{
+         //      calc= quantidade_macas*0.25;
+                   
+          //   }
+             
+               // System.out.print("o valor total da compra foi de :" + calc);
+               
+               
+               //11
+               
+       //  int sexo;
+       // double altura, pesoIdeal;
+
+      //  System.out.print("Digite sua altura em metros: ");
+       // altura = entrada.nextDouble();
+
+      //  System.out.print("Digite seu sexo (1 para feminino, 2 para masculino): ");
+       // sexo = entrada.nextInt();
+
+      //  switch (sexo) {
+       //     case 1: 
+           //     pesoIdeal = (62.1 * altura) - 44.7;
+           //     System.out.println("O peso ideal é: " + pesoIdeal + " kg");
+           //     break;
+          //  case 2: 
+            //    pesoIdeal = (72.7 * altura) - 58;
+            //    System.out.println("O peso ideal é: " + pesoIdeal + " kg");
+           //     break;
+         //   default:
+             //   System.out.println("Opção inválida!");
+     //   }
+     
+     //12
+       // int dia;
+
+     //   System.out.print("Digite um número de 1 a 7 para o dia da semana: ");
+      //  dia = entrada.nextInt();
+//
+      //  switch (dia) {
+           // case 1:
+             //   System.out.println("Domingo");
+           //    System.out.print("final de semana");
+            //    break;
+          //  case 2:
+             //  System.out.println("Segunda-feira");
+           //     System.out.print("dia de semana");
+          //     break;
+        //    case 3:
+           //     System.out.println("Terça-feira");
+           //    System.out.print("dia de semana");
+           //    break;
+                
+          //  case 4:
+          //      System.out.println("Quarta-feira");
+          //      System.out.print("dia de semana");
+          //      break;
+          //  case 5:
+          //      System.out.println("Quinta-feira");
+          //     System.out.print("dia de semana");
+          //      break;
+        //    case 6:
+         //       System.out.println("Sexta-feira");
+         //       System.out.print("dia de semana");
+         //      break;
+        //    case 7:
+        //       System.out.println("Sábado");
+         //        System.out.print("final de semana");
+          //     break;
+         //  default:
+         //  System.out.println("Número inválido! Digite de 1 a 7.");
+      //  }      
+      //13
+ 
+     //   int codigo;
+     //   double valor;
+    //
+      //  System.out.print("Digite o código (100, 101 ou 102): ");
+      // codigo = entrada.nextInt();
+
+      //  switch (codigo) {
+        ////    case 100:
+           //     produto = "Detergente";
+         //       valor = 1.59;
+             //   System.out.println(produto + " - R$ " + valor);
+             //   break;
+
+            //case 101:
+            //    produto = "Esponja";
+             //   valor = 4.59;
+            //    System.out.println(produto + " - R$ " + valor);
+              //  break;
+
+           // case 102:
+             //   produto = "Lã de aço";
+            //    valor = 1.79;
+            //    System.out.println(produto + " - R$ " + valor);
+            //    break;
+
+           // default:
+          //   System.out.println("Número inválido, digite um código de 100 a 102");
+       // }
+
+          //14
+          
+  //int operacao;
+    //   double num1, num2, resultado;
+
+      //  System.out.print("Digite o primeiro número: ");
+      //  num1 = entrada.nextDouble();
+//
+      //  System.out.print("Digite o segundo número: ");
+      //  num2 = entrada.nextDouble();
+
+      //  System.out.println("Escolha a operação:");
+      //  System.out.println("1 - Soma");
+     //   System.out.println("2 - Subtração");
+     ///   System.out.println("3 - Multiplicação");
+      //  System.out.println("4 - Divisão");
+     //   operacao = entrada.nextInt();
+
+       // switch (operacao) {
+          //  case 1:
+            //    resultado = num1 + num2;
+             //   System.out.println("Resultado: " + resultado);
+              //  break;
+
+          //  case 2:
+           //     resultado = num1 - num2;
+             //   System.out.println("Resultado: " + resultado);
+            ///    break;
+
+          //  case 3:
+          //      resultado = num1 * num2;
+           //     System.out.println("Resultado: " + resultado);
+           //     break;
+
+          //  case 4:
+           //     if (num2 != 0) {
+             //       resultado = num1 / num2;
+              //      System.out.println("Resultado: " + resultado);
+               // } else {
+            //        System.out.println("Erro: divisão por zero!");
+           //     }
+           //     break;
+
+          //  default:
+            //    System.out.println("Operação inválida!");
+     
+          
+          
+         
+             
+      }
+      
+    }
