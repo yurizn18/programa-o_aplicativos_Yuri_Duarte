0b8328b8a9b9ef38b47d99a7baf5996a691b256c
yurizn18 <duarteyurioliveira@gmail.com>
2025-09-23 20:07:53 -0300

    Add files via upload

---
+++ Atividade05_yuri_gabriel.java
@@ -0,0 +1,247 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade05_yuri_gabriel;
+
+/**
+ *
+ * @author Aluno
+ * 
+ */
+import java.util.Scanner;
+
+public class Atividade05_yuri_gabriel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada=new Scanner(System.in);
+      //  int n,i;
+        
+       // System.out.print("digite um numero");
+      //  n= entrada.nextInt();
+      // i=1;
+      //  while(i<=n){
+           //  System.out.print(i);
+         //   i++;
+         
+       //  int n,soma,quantidade;
+         
+        
+        // soma=0;
+        // quantidade=0;
+        // System.out.print("digite um número: ");
+      //   n= entrada.nextInt();
+         
+      //   while(n!=-1){
+         //    soma +=n;
+          //   quantidade++;
+             
+            // System.out.println("a soma é: " +soma);
+          //   System.out.println("e a quntidade e "+quantidade);
+             
+             //    System.out.print("Digite outro número (-1 para sair): ");
+  //  n = entrada.nextInt();
+           
+        //}
+        //3
+       // String senha,senha_c;
+     //   senha_c= "Java123";
+      //  System.out.print("digite uma senha");
+      //  senha= entrada.nextLine();
+      //  while (!senha.equals(senha_c)){
+          //  System.out.println("senha errada, tente novamente");
+           // senha= entrada.nextLine();
+           // System.out.print("senha correta");
+           
+          // int opcao=1,num1,num2,soma,subtracao,adicao,multiplicar;
+           
+          // while (opcao!=0){
+              //  System.out.println("Menu");
+               //System.out.println("1-somar");
+              // System.out.println("2-subtrair");
+              // System.out.println("3-multiplicar");
+              // System.out.println("0-sair");
+               
+              // opcao=entrada.nextInt();
+               //switch(opcao){
+                  // case 1:
+                      //System.out.print("digite um numero");
+                      //num1=entrada.nextInt();
+                       //System.out.print("digite outro numero");
+                       //num2=entrada.nextInt();
+                       //soma=num1+num2;
+                       //System.out.print("a soma é"+soma);
+                      // break;
+                       
+                       //case 2:
+                      //System.out.print("digite um numero");
+                      //num1=entrada.nextInt();
+                       //System.out.print("digite outro numero");
+                       //num2=entrada.nextInt();
+                       //subtracao=num1-num2;
+                      // System.out.print("a subtracao é"+subtracao);
+                       //break;
+                      //  case 3:
+                    //  System.out.print("digite um numero");
+                   //   num1=entrada.nextInt();
+                   //    System.out.print("digite outro numero");
+                    //   num2=entrada.nextInt();
+                    //   multiplicar=num1*num2;
+                     //  System.out.print("a subtracao é"+multiplicar);
+                  //     break;
+                       
+                       
+                    ///    case 0:
+                   //   System.out.print("saindo...");
+                   
+                   //5
+                   
+                    //    int n, fatorial, i;
+
+     //   System.out.print("Digite um número: ");
+    //    n = entrada.nextInt();
+
+     //   i = 1;
+     //   fatorial = 1;
+
+      //  while (i <= n) {
+         //   fatorial *= i;
+         ///   i++;
+     //   }
+
+      //  System.out.println("O fatorial de " + n + " é " + fatorial);
+                                   
+      //6
+    //  int n,f0,f1,proximo;
+    //  System.out.print("digite n: " );
+    //  n=entrada.nextInt();
+    //  f0=0;
+    //  f1=1;
+   //   proximo= f0+f1;
+     // while(proximo<=n){
+      //    System.out.print(","+ proximo);
+       //   f0=f1;
+     //     f1=proximo;
+     //     proximo=f0+f1;
+    //  }
+  //    
+  
+  //7
+ /*int n,i;
+        
+        System.out.print("Insira um número maior que 2: ");
+        n = entrada.nextInt();
+               
+        while (n % 2 == 0) {
+            System.out.print("Não é um número primo.");
+            System.out.print("Insira um número maior que 2: ");
+            n = entrada.nextInt();
+        }
+        System.out.print("Primo");*/
+ 
+ //8
+ /*int n;
+ int soma=0;
+ System.out.print("digite um numero maior que zero: ");
+ n= entrada.nextInt();
+ while(n>=0){
+     soma+=n;
+     
+     System.out.println("a soma é: " +soma);
+      System.out.print("Digite outro número (-1 para sair): ");
+    n = entrada.nextInt();*/
+ 
+ //9  
+ /*int n;
+ 
+        int interacoes = 0;
+
+        System.out.print("Digite um número (negativo para sair): ");
+        n = entrada.nextInt();
+
+        while (n >= 0) {
+            int soma = 0;
+
+            while (n > 0) {
+                soma += n % 10;
+                n = n / 10;
+            }
+
+            System.out.println("Soma dos dígitos: " + soma);
+            interacoes++;
+
+            System.out.print("Digite um número (negativo para sair): ");
+            n = entrada.nextInt();
+        }
+
+        System.out.println("Total de interações: " + interacoes);
+        entrada.close();
+
+//10
+ /*int n,par,impar;
+       
+       System.out.print("Insira um número maior que 0: ");
+       n = entrada.nextInt();
+       
+       par = n/2;
+       impar = 3*n+1;
+       
+       while (n>0){
+           if (n % 2 == 0) {
+               par = n/2;
+               System.out.print("O valor da formula do número par é: "+par);
+           }else{
+               impar = 3*n+1;
+               System.out.print("O valor da formula do número impar: "+impar);
+           }
+           
+           System.out.print("Insira um número maior que 0: ");
+           n = entrada.nextInt();          
+           
+       }*/
+
+    }
+
+
+ 
+     
+ 
+   
+      
+  
+  
+  
+  
+                                   
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+   
+        
+      
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+        
+    
+    }
+    
+
