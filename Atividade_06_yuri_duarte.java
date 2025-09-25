7874cfc839313ddaefa940413eef427d090e6eb3
yurizn18 <duarteyurioliveira@gmail.com>
2025-09-25 19:48:09 -0300

    Add files via upload

---
+++ Atividade_06_yuri_duarte.java
@@ -0,0 +1,190 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade_06_yuri_duarte;
+import java.util.Scanner;
+import java.util.Random;
+import java.lang.Math;
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade_06_yuri_duarte {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada=new Scanner(System.in);
+       // int n;
+        
+       //do{
+         //   System.out.print("digite um nota");
+           // n=entrada.nextInt();
+            
+            
+        //}while(n>10){
+        //System.out.print("nota valida"+n);
+        
+       //  int n;
+      //  int soma = 0;
+      //  int quantidade = 0;
+     //   double media = 0.0;
+
+      //  do {
+       //     System.out.print("Digite um número (0 para sair): ");
+        //    n = entrada.nextInt();
+
+         
+      //      if (n > 0) {
+        //        soma += n;
+      //          quantidade++;
+      //      }
+     //   } while (n> 0); 
+     //   if (quantidade > 0) {
+       //     media = soma / quantidade; 
+       //     System.out.println("Soma: " + soma);
+       //     System.out.println("Quantidade: " + quantidade);
+      //      System.out.println("Média: " + media);
+      //  } else {
+       //     System.out.println("Nenhum número válido foi digitado.");
+  
+       // }
+
+        //3
+ // int n;
+      //  int tentativas = 0;
+      // // int n_a = (int)(Math.random() * 100) + 1;
+
+      //  do {
+       //     System.out.print("Digite um número entre 1 e 100: ");
+        //    n = entrada.nextInt();
+       //     tentativas++;
+
+       //     if (n > n_a) {
+        //        System.out.println("Muito alto!");
+         //   } else if (n < n_a) {
+        //        System.out.println("Muito baixo!");
+       //     } else {
+          //      System.out.println("Você acertou!");
+          //  }
+
+      //  } while (n != n_a);
+
+      //  System.out.println("Parabéns! O número secreto é " + n_a + ".");
+       // System.out.println("Você acertou em " + tentativas + " tentativas.");
+
+       
+       //4
+        
+
+      //  int n;
+      //  char op;
+
+      //  do {
+        //    System.out.print("Digite um número inteiro: ");
+         //   n = entrada.nextInt();
+
+         //   if (n > 0) {
+           //     for (int i = 0; i < n; i++) {
+           //         System.out.print(i + " "); 
+           //     }
+           //    System.out.println(); 
+           // } else {
+         //       System.out.println("Número inválido.");
+         //   }
+
+          //  System.out.print("Digite 's' para continuar ou 'n' para parar: ");
+         //   op = entrada.next().charAt(0);
+
+       // } while (op == 's');
+       //5
+
+  
+  // String senhaDigitada;
+     //   String senhaCorreta = "senai123";
+    //    int tentativas = 3;
+
+    //    do {
+       //     System.out.print("Digite a senha: ");
+        //    senhaDigitada = entrada.nextLine();
+
+         //   if (senhaDigitada.equals(senhaCorreta)) {
+             //   System.out.println("Senha correta! Acesso liberado.");
+              //  break;
+          //  } else {
+          //      tentativas--;
+          //      if (tentativas > 0) {
+             //       System.out.println("Senha incorreta. Você tem " + tentativas + " tentativa(s) restante(s).");
+            //    } else {
+            //        System.out.println("Você excedeu o número de tentativas. Acesso bloqueado.");
+            //    }
+         //   }
+
+      //  } while (tentativas > 0);
+      
+      //6
+    //  int n,resultado;
+    //  System.out.println("digite um  numero para ver a tabuada: ");
+    //  n= entrada.nextInt();
+     // for(int i=1;i<=10;i++){
+        //    resultado= n*i;
+        //  System.out.println("o resultado da conta é" +n+"x"+i+"="+resultado);
+          
+         
+      //}
+      
+      //7
+   //   int n;
+             
+    //System.out.print("Digite um número maior que 0: ");
+      //   n = entrada.nextInt();
+
+       // int somaPares = 0;
+      //  int somaImpares = 0;
+
+    //    for (int i = 1; i <= n; i++) {
+        //    if (i % 2 == 0) {
+      //          somaPares += i;
+      //          System.out.println(i + " é par");
+      //      } else {
+       //         somaImpares += i;
+          //      System.out.println(i + " é ímpar");
+         //   }
+    //   }
+
+      //  System.out.println("\nSoma dos números pares: " + somaPares);
+     //   System.out.println("Soma dos números ímpares: " + somaImpares);
+
+      //  entrada.close();
+      
+      //8
+ //int n;
+        //System.out.print("Digite um número maior que 1: ");
+        //n = entrada.nextInt();
+
+        //if (n <= 1) {
+          //  System.out.println("Número inválido");
+        //} else {
+            //System.out.print("Números primos até " + n + ": ");
+            //for (int i = 2; i <= n; i++) {
+                //boolean primo = true;
+
+                //for (int k = 2; k * k <= i; k++) {
+                    //if (i % k == 0) {
+                     //   primo = false;
+                   //     break;
+                 //   }
+               // }
+
+             //   if (primo) {
+           //         System.out.print(i + " ");
+         //       }
+       //     }
+     //   }
+
+    
+    }
+}
